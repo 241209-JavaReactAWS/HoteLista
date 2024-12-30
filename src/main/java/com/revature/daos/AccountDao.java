4ee3033f23bf2503fcb4e38510212cbca7dfcbee
Sanjana Sidgiddi <sansid247700@gmail.com>
2024-12-30 10:48:53 -0500

    Completed Account Model class and initialized JpaRepository AccountDAO

--- src/main/java/com/revature/daos/AccountDao.java
+++ src/main/java/com/revature/daos/AccountDao.java
@@ -1,5 +1,9 @@
 package com.revature.daos;
 
-public class AccountDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.revature.models.Account;
+
+public interface AccountDao extends JpaRepository<Account, Integer> {
     
 }
--- src/main/java/com/revature/models/Account.java
+++ src/main/java/com/revature/models/Account.java
@@ -1,5 +1,53 @@
 package com.revature.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Account {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int accountId;
+    private String email;
+    private String password;
+    private boolean isOwner;
+
+    public Account(int accountId, String email, String password, boolean isOwner) {
+        this.accountId = accountId;
+        this.email = email;
+        this.password = password;
+        this.isOwner = isOwner;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getRole() {
+        return isOwner;
+    }
+
+    public void setRole(boolean isOwner) {
+        this.isOwner = isOwner;
+    }
 }
