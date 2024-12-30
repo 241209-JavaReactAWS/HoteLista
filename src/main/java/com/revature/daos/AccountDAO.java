d2d41db5e22c1c2989170ec511899fd5485503ef
Sanjana Sidgiddi <sansid247700@gmail.com>
2024-12-30 16:05:23 -0500

    Added service methods to Account with optional values

--- src/main/java/com/revature/daos/AccountDAO.java
+++ src/main/java/com/revature/daos/AccountDAO.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountDAO extends JpaRepository<Account, Integer>{
-    Account findByEmail(String email);
+    Optional<Account> findByEmail(String email);
     List<Account> findByRole(boolean isOwner);
 }
--- src/main/java/com/revature/services/AccountService.java
+++ src/main/java/com/revature/services/AccountService.java
@@ -1,6 +1,7 @@
 package com.revature.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,9 +28,16 @@ public class AccountService {
         return accountDAO.findAll();
     }
 
+    /** Look up (Filter/Search) Accounts by accountId */
+    public Account searchById(int accountId){
+        Optional<Account> acc = accountDAO.findById(accountId);
+        return acc.orElse(null);
+    }
+
     /** Look up (Filter/Search) Accounts by email */
     public Account searchByEmail(String email){
-        return accountDAO.findByEmail(email);
+        Optional<Account> acc = accountDAO.findByEmail(email);
+        return acc.orElse(null);
     }
 
     /** Look up (Filter/Search) Accounts by role type: user or hotel owner */
