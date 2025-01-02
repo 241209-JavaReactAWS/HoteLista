cce24e01a5ff7d84548ea25eca6e4b7fcfda549a
Sanjae Chin <sanjae.chin@gmail.com>
2025-01-02 11:03:16 -0500

    Updated hotelreview classes

--- src/main/java/com/revature/controllers/HotelReviewController.java
+++ src/main/java/com/revature/controllers/HotelReviewController.java
@@ -1,16 +1,47 @@
 package com.revature.controllers;
 
+import com.revature.exceptions.hotel.NotFoundHotelException;
+import com.revature.exceptions.hotelreview.NotFoundHotelReviewException;
+import com.revature.models.HotelReview;
 import com.revature.services.HotelReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-// TODO: Add CrossOrign with credetials and modify endpoints accordingly
+@RequestMapping("/hotel/{hotelId}/reviews")
 public class HotelReviewController {
     private final HotelReviewService hotelReviewService;
 
     @Autowired
     public HotelReviewController(HotelReviewService hotelReviewService) {
         this.hotelReviewService = hotelReviewService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<HotelReview> addHotelReview(@PathVariable int hotelId, @RequestBody HotelReview hotelReview){
+        try{
+            HotelReview newHotelReview = hotelReviewService.addHotelReview(hotelId, hotelReview);
+            return ResponseEntity.ok(newHotelReview);
+        }catch (NotFoundHotelException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<Void> deleteHotelReview(@PathVariable int hotelId, @PathVariable int reviewId){
+        hotelReviewService.deleteHotelReview(reviewId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<HotelReview> updateHotelReview(@PathVariable int hotelId, @PathVariable int reviewId, @RequestBody HotelReview hotelReview){
+        try{
+            HotelReview updatedHotelReview = hotelReviewService.updateHotelReview(hotelReview);
+            return ResponseEntity.ok(updatedHotelReview);
+        } catch (NotFoundHotelReviewException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
--- src/main/java/com/revature/services/HotelReviewService.java
+++ src/main/java/com/revature/services/HotelReviewService.java
@@ -1,6 +1,7 @@
 package com.revature.services;
 
 import com.revature.daos.HotelReviewDAO;
+import com.revature.exceptions.hotel.NotFoundHotelException;
 import com.revature.exceptions.hotelreview.NotFoundHotelReviewException;
 import com.revature.models.HotelReview;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,28 +11,32 @@ import java.util.Optional;
 
 @Service
 public class HotelReviewService {
-    private final HotelReviewDAO hotelReviewDao;
+    private final HotelReviewDAO hotelReviewDAO;
+    private final HotelServices hotelServices;
 
     @Autowired
-    public HotelReviewService(HotelReviewDAO hotelReviewDao) {
-        this.hotelReviewDao = hotelReviewDao;
+    public HotelReviewService(HotelReviewDAO hotelReviewDAO, HotelServices hotelServices) {
+        this.hotelReviewDAO = hotelReviewDAO;
+        this.hotelServices = hotelServices;
     }
 
-    public HotelReview addHotelReview(HotelReview hotelReview){
-        return hotelReviewDao.save(hotelReview);
+    public HotelReview addHotelReview(int hotelId, HotelReview hotelReview) throws NotFoundHotelException {
+        hotelServices.findHotelByID(hotelId);
+        return hotelReviewDAO.save(hotelReview);
     }
 
     public void deleteHotelReview(int hotelReviewId){
-        hotelReviewDao.deleteById(hotelReviewId);
+        hotelReviewDAO.deleteById(hotelReviewId);
     }
 
     public HotelReview updateHotelReview(HotelReview hotelReview) throws NotFoundHotelReviewException{
-        Optional<HotelReview> optionalHotelReview = hotelReviewDao.findById(hotelReview.getHotelReviewId());
+        Optional<HotelReview> optionalHotelReview = hotelReviewDAO.findById(hotelReview.getHotelReviewId());
 
         if(optionalHotelReview.isEmpty()) {
             throw new NotFoundHotelReviewException();
         }
 
-        return hotelReviewDao.save(hotelReview);
+        return hotelReviewDAO.save(hotelReview);
     }
+
 }
